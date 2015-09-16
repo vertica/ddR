25786de7e143718300246f8a4d31945c501bf588
DistR <guptavis@ubuntu.(none)>
2015-09-16 13:51:18 -0700

    updating the code to test/run kmeans to work with new api

--- scripts/run_kmeans.R
+++ scripts/run_kmeans.R
@@ -1,29 +1,32 @@
 library(HPdcluster)
-
 library(dds)
 
 #Uncomment the following lines to use Distributed R 
-#library(distributedR.dds)
+library(distributedR.dds)
+
 #useBackend(distributedR)
+
 
 nInst = 4  #determines the no. of  partitions (and hence max cores utilized)
 ncol = 10
-nrow = 1000000
+nrow = 10000000
 K = 10
 centers = 100*matrix(rnorm(K*ncol),nrow = K)
 cat("Generating data with rows=",nrow," and cols=", ncol,"\n")
 nrow = as.integer(nrow/nInst)
 
-generateKMeansData <- function(id,centers, nrow, ncol) {
+generateKMeansData <- function(id, centers, nrow, ncol) {
 offsets = matrix(rnorm(nrow*ncol),nrow = nrow,ncol = ncol)
 cluster_ids = sample.int(nrow(centers),nrow,replace = TRUE)
 feature_obs = centers[cluster_ids,] + offsets
-list(feature_obs)
+feature_obs
 }
 
 
-feature <- dmapply(generateKMeansData, as.list(1:nInst), 
-                MoreArgs = list(centers = centers, nrow = nrow, ncol = ncol))
+feature <- dmapply(generateKMeansData,id = 1:nInst,
+                MoreArgs = list(centers = centers, nrow = nrow, ncol = ncol),
+		output.type = "DArrayClass", 
+		combine = "row", nparts = c(nInst,1))
 
 model <- hpdkmeans(feature,K, trace = TRUE) 
 
