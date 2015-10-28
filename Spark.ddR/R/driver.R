f03a9bf5e93909d13bbf578eefe71ac924f17572
Edward Ma <etduwx@gmail.com>
2015-10-28 15:06:18 -0700

    Initial skeleton code for SparkR.ddR

--- Spark.ddR/R/driver.R
+++ Spark.ddR/R/driver.R
@@ -15,4 +15,50 @@
 # Boston, MA 02111-1307 USA.
 ###################################################################
 
-#' @import methods ddR SparkR
+#' @import methods ddR 
+#' @importFrom SparkR sparkR.init sparkR.stop 
+
+Spark.ddR.env <- new.env()
+
+# Create distributedR ddRDriver
+setClass("SparkRddR", contains="ddRDriver")
+
+#' @export 
+# Exported Driver
+SparkR <- new("SparkRddR",DListClass = "ddR_RDD",DFrameClass = "ddR_RDD",DArrayClass = "ddR_RDD",backendName = "SparkR")
+
+#' @export
+setMethod("init","SparkRddR",
+  function(x,...) {
+    message("Backend switched to SparkR. Initializing the Spark context...")
+    Spark.ddR.env$context <- sparkR.init()
+    ## TODO(etduwx): return the actual number of executors
+    return (1)
+  }
+)
+
+#' @export
+setMethod("shutdown","SparkRddR",
+  function(x) {
+    message("Switching out of using SparkR. Shutting it down...")
+    sparkR.stop()
+  }
+)
+
+#' @export
+setMethod("do_dmapply",
+           signature(driver="SparkRddR",func="function"),
+           function(driver,func,...,MoreArgs=list(),
+                    output.type =
+                      c("dlist","dframe","darray","sparse_darray"),
+                    nparts=NULL,
+                    combine=c("default","c","rbind","cbind")) {
+
+  ## Placeholder
+  rdd <- SparkR:::parallelize(Spark.ddR.env$context, 1:10, prod(nparts))
+ 
+  if(output.type == "dlist") num.dims <- 1
+  else num.dims <- 2
+
+  new("ddR_RDD", RDD = rdd, nparts=nparts, psize=matrix(1,prod(nparts), num.dims), partitions = 1:prod(nparts))
+})
---
+++ Spark.ddR/R/object.R
@@ -0,0 +1,84 @@
+###################################################################
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+# This program is free software; you can redistribute it 
+# and/or modify it under the terms of the GNU General Public 
+# License, version 2 as published by the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License 
+# along with this program; if not, write to the Free Software 
+# Foundation, Inc., 59 Temple Place, Suite 330, 
+# Boston, MA 02111-1307 USA.
+###################################################################
+
+#' @include driver.R
+
+# Create the ddR_RDD Object
+setClass("ddR_RDD",contains="DObject",
+    slots=list(RDD = "RDD", partitions = "numeric"),
+    prototype = prototype(nparts = c(1L,1L), psize = matrix(1,1),
+      dim = c(1L,1L)
+))
+
+setMethod("initialize", "ddR_RDD", function(.Object, ...) {
+   .Object <- callNextMethod(.Object, ...)
+})
+
+#' @export
+setMethod("get_parts",signature("ddR_RDD","missing"),
+  function(x, ...){
+  index = seq(length(x@partitions))
+ lapply(index,function(b) {
+      new("ddR_RDD",RDD = x@RDD, partitions = x@partitions[b],dim = as.integer(x@psize[b,]),psize=matrix(x@psize[b,],nrow=1,ncol=length(x@psize[b,])))
+    })
+}
+)
+
+#' @export
+setMethod("get_parts",signature("ddR_RDD","integer"),
+  function(x, index, ...){
+   lapply(index,function(b) {
+      new("ddR_RDD",RDD = x@RDD, partitions = x@partitions[b],dim = as.integer(x@psize[b,]),psize=matrix(x@psize[b,],nrow=1,ncol=length(x@psize[b,])))
+    } )
+  }
+)
+
+#' @export
+setMethod("do_collect",signature("ddR_RDD","integer"),
+  function(x, parts) {
+    if(identical(x@partitions, parts) && length(parts) > 1) {
+       SparkR:::collect(x@RDD)
+    } else if(length(parts) > 1) {
+      stop("Cannot collect on more than one index at a time")
+    }
+   else {
+      SparkR:::collect(x@RDD,x@partitions[[parts]])
+   }
+})
+
+setMethod("combine",signature(driver="ddR_RDD",items="list"),
+  function(driver,items){
+    split_indices <- sapply(items,function(x) {
+      x@partitions
+    })
+    dims <- sapply(items,function(x) {
+      x@dim
+    })
+
+    psizes <- sapply(items,function(x) {
+      x@psize
+    })
+
+    if(is.matrix(dims)) dims <- colSums(dims)
+    else dims <- sum(dims)
+
+    psizes <- as.matrix(psizes)
+    rownames(psizes) <- NULL
+
+    new("ddR_RDD",RDD=items[[1]]@RDD,partitions = unlist(split_indices), dim = dims, psize = psizes)
+  }
+)
