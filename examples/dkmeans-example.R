4d2deada96e653b85855df8073fc6f715fb9a67e
Clark Fitzgerald <clarkfitzg@gmail.com>
2016-07-05 17:42:05 -0700

    modifying kmeans example

--- examples/dkmeans-example.R
+++ examples/dkmeans-example.R
@@ -2,35 +2,44 @@ library(ddR)
 library(kmeans.ddR)
 
 nInst = 2 # Change level of parallelism
-useBackend(parallel,executors = nInst)
+useBackend(parallel, executors = nInst)
 # Uncomment the following lines to use Distributed R 
 #library(distributedR.ddR)
 #useBackend(distributedR)
 
 # Set up data size
-ncol = 100
-nrow = 1000000
+numcols = 100
+numrows = 100000
 K = 10
-centers = 100*matrix(rnorm(K*ncol),nrow = K)
-cat("Generating data with rows=",nrow," and cols=", ncol,"\n")
-nrow = as.integer(nrow/nInst)
+
+set.seed(37)
+centers = matrix(rnorm(K * numcols, sd = 10), nrow = K)
+dnumrows = as.integer(numrows/nInst)
 
 generateKMeansData <- function(id, centers, nrow, ncol) {
-offsets = matrix(rnorm(nrow*ncol),nrow = nrow,ncol = ncol)
-cluster_ids = sample.int(nrow(centers),nrow,replace = TRUE)
-feature_obs = centers[cluster_ids,] + offsets
-feature_obs
+    offsets = matrix(rnorm(dnumrows * numcols), nrow = nrow, ncol = ncol)
+    cluster_ids = sample.int(nrow(centers),dnumrows,replace = TRUE)
+    feature_obs = centers[cluster_ids,] + offsets
+    feature_obs
 }
 
-feature <- dmapply(generateKMeansData,id = 1:nInst,
-                MoreArgs = list(centers = centers, nrow = nrow, ncol = ncol),
+cat(sprintf("Generating %d x %d matrix for clustering with %d means\n",
+            numrows, numcols, K))
+
+dfeature <- dmapply(generateKMeansData,id = 1:nInst,
+  MoreArgs = list(centers = centers, nrow = dnumrows, ncol = numcols),
 		output.type = "darray", 
 		combine = "rbind", nparts = c(nInst,1))
 
-training_time <- system.time({model <- dkmeans(feature,K)})[3]
-cat("training dkmeans model on distributed data: ", training_time,"\n")
+cat("training dkmeans model on distributed data\n")
+dtraining_time <- system.time(
+    dmodel <- dkmeans(dfeature, K, iter.max = 100)
+)[3]
+cat(dtraining_time, "\n")
 
-feature <- collect(feature)
-training_time <- system.time({model <- kmeans(feature, K, algorithm = "Lloyd")})[3]
-cat("training normal kmeans model on centralized data: ", training_time,"\n")
-
+feature <- collect(dfeature)
+cat("training normal kmeans model on centralized data\n")
+training_time <- system.time(
+    model <- kmeans(feature, K, iter.max = 100, algorithm = "Lloyd")
+)[3]
+cat(training_time, "\n")
