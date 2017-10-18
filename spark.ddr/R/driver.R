adad19bb1be23bfc01e8e855d0791ad0099f83fe
Krishna Kalyan <krishnakalyan3@gmail.com>
2017-10-18 16:35:03 +0200

    set spark context

--- spark.ddr/R/driver.R
+++ spark.ddr/R/driver.R
@@ -1,15 +1,12 @@
+#' @import methods ddR
+#' @importFrom sparklyr spark_connect
 
-
-
-setClass("spark.ddR", contains = "ddRDriver")
+setClass("spark.ddR", contains = "ddRDriver", slots = c(sc = "spark_connection"))
 
 init_spark <- function(master = "local", ...) {
   message("Backend switched to Spark. Initializing the Spark context...")
 
   sc = sparklyr::spark_connect(master , ...)
-
-  # TODO
-  # Executors
 
   new("spark.ddR",
       DListClass = "ParallelObj",
